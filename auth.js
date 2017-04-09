20f4a2998e26f35b1045602e1dda95908fd93201
Jarik Bulba <jarik-bulba@rambler.ru>
2017-04-09 14:03:29 +0300

    Chenget auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 function authenticate (login, password) {
-	if(login === "login" && password === "password") {
+	if(login === "login") {
 	return 'You were logged in';
 	} else {
 	return 'Login is not correct'
